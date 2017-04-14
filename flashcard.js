63e9e17c282c9af467ad59cce6a9fda728fad8f1
Monica Jones <monicaj314@yahoo.com>
2017-04-13 23:39:02 -0500

    added create basic command to add a basic card to the flashcard array

--- flashcard.js
+++ flashcard.js
@@ -1,41 +1,49 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
-var jsonFile = 'flashcard.json'
+var jsonFile = './flashcard.json';
 var data = fs.readFileSync(jsonFile);
 var flashcards = JSON.parse(data);
-
+var obj = require(jsonFile);
 var input = process.argv;
 
+var flashcardIndex = 0;
+function startFlashcards() {
+    if(input[2] == null){
+        inquirer.prompt([{
+        type: "input",
+        message: flashcards[flashcardIndex].front,
+        name: "Flashcard"
+        }]).then(function(answer) {
+            if (answer.Flashcard == flashcards[flashcardIndex].back) {
+                console.log("Correct!");
+                flashcardIndex++;
+                startFlashcards();
+            } else {
+                console.log("Incorrect. The correct answer is " + flashcards[flashcardIndex].back);
+                flashcardIndex++;
+                startFlashcards();
+            }
+       });
+    };
+};
+
+
+// Everything below this line is still in progress
+
+//For creating card via command line
+
+function BasicCard(front, back)  {
+    this.front = front;
+    this.back = back;
+};
+
+// Cloze feature 
 function ClozeCard(text, cloze) {
     this.text = text;
     this.cloze = cloze; 
 };
 
-
 var createCloze = new ClozeCard(this.full, this.blank);
-
-// Revising this later for creating new flashcards through node command
-// var obj = {
-//     dataArray: []
-// };
-// var json = JSON.stringify(obj);
-
-// function pushCard() {
-//     fs.appendFile(jsonFile, json, 'utf8', callback)
-// }
-// Prompts for creating new flashcards via command line
-var ifBasic = [{
-        type: "input",
-        message: "Enter your question.",
-        name: "front"
-    },
-    {
-        type: "input",
-        message: "Enter the answer.",
-        name: "back"
-    } 
-];
-
 var ifCloze = [{
         type: "input",
         message: "Enter your cloze statement.",
@@ -47,15 +55,34 @@ var ifCloze = [{
         name: "cloze"
     }
     ];
-// Function for creating new flashcards via command line
-    function makeFlashcards() {
+
+// Use "create" command to add new basic or cloze flashcards
+
+function makeFlashcards() {
     if (input[2] == "create" && input[3] == "basic") {
-        inquirer.prompt(ifBasic, function(answers) {
+        inquirer.prompt([{
+            type: "input",
+            message: "Enter your question.",
+            name: "front"
+        },
+        {
+            type: "input",
+            message: "Enter the answer.",
+            name: "back"
+
+        }]).then(function(answers) {
             var basicFront = answers.front;
             var basicBack = answers.back;
-            createBasic(basicFront, basicBack);
-            });
-        } else if (input[2] == "create" && input[3] == "cloze") {
+            var createBasic = new BasicCard(basicFront, basicBack);
+            createBasic;
+            console.log(createBasic);
+            
+            flashcards.push(JSON.stringify(createBasic));
+            console.log("Basic flashcards updated.")
+            console.log(flashcards);
+            
+        });
+    } else if (input[2] == "create" && input[3] == "cloze") {
             inquirer.prompt(ifCloze, function(answers) {
             var clozeFront = answers.text;
             var clozeBack = answers.cloze;
@@ -64,47 +91,13 @@ var ifCloze = [{
         }  
     };
 
-//Start game and loop through all the basic questions. 
-//For now, prompts next question on user input 
+makeFlashcards();
 
-//function startFlashcards() {
-    // var allCards = flashcards.map(function(card, i) {
-    // return {
-    //             type: "input",
-    //             message: card.front,
-    //             name: "Flashcard" + card[i]
-    //     };  
-    // });
-    //console.log(flashcards[0].front);
-
-function BasicCard(front, back)  {
-    this.front = front;
-    this.back = back;
-};
-
-
-var createBasic = new BasicCard(this.front, this.back); 
-
-var flashcardIndex = 0;
-function startFlashcards() {
-
-    inquirer.prompt([{
-    type: "input",
-    message: flashcards[flashcardIndex].front,
-    name: "Flashcard"
-    }]).then(function(answer) {
-    if (answer.Flashcard == flashcards[flashcardIndex].back) {
-        console.log("Correct!");
-        flashcardIndex++;
-        startFlashcards();
-    } else {
-        console.log("Incorrect. The correct answer is " + flashcards[flashcardIndex].back);
-        flashcardIndex++;
-        startFlashcards();
-    }
-    });
-};
+// Run the file to start flashcard game
 
 startFlashcards();
 
-    
+
+
+
+
